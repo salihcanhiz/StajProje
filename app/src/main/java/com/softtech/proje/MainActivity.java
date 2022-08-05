b1aa4e6330d4e0b47054e747cac5d9bdc9b5dca8
salihcanhiz <salihcanhiz20@gmail.com>
2022-08-05 12:07:19 +0300

    Staj-Proje

---
+++ app/src/main/java/com/softtech/proje/MainActivity.java
@@ -0,0 +1,39 @@
+package com.softtech.proje;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.graphics.drawable.GradientDrawable;
+import android.os.Bundle;
+import android.service.autofill.OnClickAction;
+import android.view.Gravity;
+import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+        findViewById(R.id.imageMenu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+        NavigationView navigationView = findViewById(R.id.navigationView);
+
+        NavController navController = Navigation.findNavController(this, R.id.navHostFragment);
+
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+}
---
+++ app/src/main/java/com/softtech/proje/Muzeler.java
@@ -0,0 +1,14 @@
+package com.softtech.proje;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Muzeler extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_muzeler);
+    }
+}
